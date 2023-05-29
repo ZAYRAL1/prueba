caeea2cc7b51619fb8afe3d7395ef04961ff3e4e
ZAYRAL1 <134566015+ZAYRAL1@users.noreply.github.com>
2023-05-28 21:46:27 -0600

    Add files via upload

--- administrador.php
+++ administrador.php
@@ -1,12 +1,13 @@
 <HTML>
-    <HEAD>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <TITLE>
-            Globos y Sorpresas
-        </TITLE>
-        <script type="text/javascript">
+
+<HEAD>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <TITLE>
+        Globos y Sorpresas
+    </TITLE>
+    <script type="text/javascript">
         function confirmarEliminacion() {
             return confirm("Esta seguro de que desea eliminar este elemento?");
         }
@@ -18,82 +19,84 @@
     </script>
 
 
-        <script>
-            function valor() {
-                var nombre;
-                nombre = prompt("Introduce tu Nombre:", " ");
-                alert("hola" + " " + nombre + "" + "," + "ESTAS EN LA VISTA DE ADMINISTRADOR");
-            }
-            document.querySelector('form').addEventListener('submit',
-                function (event) {
-                    if (!document.getElementById('eliminarproductos').checked) {
-                        alert('Debes seleccionar algun producto');
-                        event.preventDefault();
-                    }
-                });
-        </script>
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet"
-            integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
-        <link rel="stylesheet" type="text/css" href="estilos1.css" />
-        <link rel="shortcut icon" href="logo icono.png">
-        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha1/css/bootstrap.min.css"
-            integrity="sha384-6mVEWLZGpUgMhU6KdW8rO7X9RgO+QrKGhCw/voYGZz4YRIZT/7bQjNkxzsGIPxNl" crossorigin="anonymous">
-        <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"
-            integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj"
-            crossorigin="anonymous"></script>
-        
-            <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.3/dist/umd/popper.min.js"></script>
-        <script src="https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha1/js/bootstrap.min.js"
-            integrity="sha384-FpVtM5eYFx5n0xLTQOxuL7KgPFidWXSJTH5fzveKZ5V5yYok5K7XgpXjKkRS7JN/"
-            crossorigin="anonymous"></script>
-        <div id="verde" style="background-color:white; font-style:Verdana">
-            <A HREF="globosysorpresas.html">SALIR</A>
-            <div class="productos menu">
-                <div class="row">
-                    <img src="./productos/menu.png" height="20px" alt="menu">
-                </div>
-                <div class="container">
-                    <section class="administrador row">
-                        <CENTER>
-                            <h1>ADMINISTRADOR DE GLOBOS Y SORPRESAS</h1>
-                        </CENTER>
-                        <hr size="4px" color="white " />
-                        
-    
-    <body style="background-color: white">
-        </div>
-        </div>
-        </div>
-            </head>
-    
-        <BODY>
-    
-            <body style="background-color:white;">
-            </body>
-            <div id="amarillo" style="background-color:lightblue; font-style:arial">
-                <h2>
-                    <CENTER>
-                        <div id="amarillo" style="background-color: lightcoral"></div>
-                    </CENTER>
-                    <hr size="1px" color="lightgrey" />
-                    <CENTER><img src="./productos/globo-y-bomba-imagen-animada-0009.gif"></CENTER>
+    <script>
+        function valor() {
+            var nombre;
+            nombre = prompt("Introduce tu Nombre:", " ");
+            alert("hola" + " " + nombre + "" + "," + "ESTAS EN LA VISTA DE ADMINISTRADOR");
+        }
+        document.querySelector('form').addEventListener('submit',
+            function (event) {
+                if (!document.getElementById('eliminarproductos').checked) {
+                    alert('Debes seleccionar algun producto');
+                    event.preventDefault();
+                }
+            });
+    </script>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="estilos1.css" />
+    <link rel="shortcut icon" href="logo icono.png">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha1/css/bootstrap.min.css"
+        integrity="sha384-6mVEWLZGpUgMhU6KdW8rO7X9RgO+QrKGhCw/voYGZz4YRIZT/7bQjNkxzsGIPxNl" crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"
+        integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj"
+        crossorigin="anonymous"></script>
+
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.3/dist/umd/popper.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha1/js/bootstrap.min.js"
+        integrity="sha384-FpVtM5eYFx5n0xLTQOxuL7KgPFidWXSJTH5fzveKZ5V5yYok5K7XgpXjKkRS7JN/"
+        crossorigin="anonymous"></script>
+    <div id="verde" style="background-color:white; font-style:Verdana">
+        <A HREF="globosysorpresas.html">SALIR</A>
+        <div class="productos menu">
+            <div class="row">
+                <img src="./productos/menu.png" height="20px" alt="menu">
             </div>
+            <div class="container">
+                <section class="administrador row">
+                    <CENTER>
+                        <h1>ADMINISTRADOR DE GLOBOS Y SORPRESAS</h1>
+                    </CENTER>
+                    <hr size="4px" color="white " />
+
+
+<body style="background-color: white">
+    </div>
+    </div>
+    </div>
+    </head>
+
+    <BODY>
+
+        <body style="background-color:white;">
+        </body>
+        <div id="amarillo" style="background-color:lightblue; font-style:arial">
+            <h2>
+                <CENTER>
+                    <div id="amarillo" style="background-color: lightcoral"></div>
+                </CENTER>
+                <hr size="1px" color="lightgrey" />
+                <CENTER><img src="./productos/globo-y-bomba-imagen-animada-0009.gif"></CENTER>
+        </div>
 
 
 
 
-            <CENTER><form method="post" action="consultar1.php">
-		<label for="IDproducto">ID PRODUCTO:</label>
-		<input type="text" id="INSERTA ID PRODUCTO" name="IDproducto"><br><br>
+        <CENTER>
+            <form method="post" action="consultar1.php">
+                <label for="IDproducto">ID PRODUCTO:</label>
+                <input type="text" id="INSERTA ID PRODUCTO" name="IDproducto"><br><br>
 
-		<input type="submit" value="Consultar">
-	</form></CENTER>
+                <input type="submit" value="Consultar">
+            </form>
+        </CENTER>
 
 
-
-    <?php
+        <?php
     include("conexiontienda.php");
     $result = mysqli_query ($conexion, "SELECT * FROM productos");
+
     
 
 echo
@@ -127,7 +130,7 @@ echo
            <input type='submit' value='Eliminar Producto'>
            </form></td>
 
-           <td> <BR><form method='post' action='modificarej.php'onsubmit='return confirmarActualizar()'> \n
+           <td> <BR><form method='post' action='actualizartienda.php'onsubmit='return confirmarActualizar()'> \n
            <input type='hidden' name='IDproducto' value='".$consulta['IDproducto']."'><BR>
            <input type='submit' name='Actualizar' value='Actualizar Producto'>
            </form></td>
@@ -145,13 +148,5 @@ echo
     echo "</table>";
     
 ?>
-
-
-
-
-
-
-
-
-
-
+</head>
+</HTML>
